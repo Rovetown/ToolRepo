63c9871ded79bae6129c2bce1ba48bb0a6a5e9c3
Rovetown <110688035+Rovetown@users.noreply.github.com>
2025-05-14 23:10:02 +0200

    Added Scaffold Python GTK Frontend .gitignore

--- backend/main.go
+++ backend/main.go
@@ -1,19 +1,14 @@
 package main
 
 import (
-    "fmt"
-    "net/http"
-
-    "github.com/diamondburned/gotk4/pkg/gtk/v4"
-    "github.com/gomarkdown/markdown"
-    "github.com/gomarkdown/markdown/html"
-    "github.com/gomarkdown/markdown/parser"
+	"fmt"
+	"net/http"
 )
 
 func main() {
-    http.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
-        fmt.Fprintln(w, "OK")
-    })
-    // TODO: add caching, Markdown fetch, WebKitGTK API, etc.
-    http.ListenAndServe(":8080", nil)
+	http.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintln(w, "OK")
+	})
+	// TODO: add caching, Markdown fetch, WebKitGTK API, etc.
+	http.ListenAndServe(":8080", nil)
 }
